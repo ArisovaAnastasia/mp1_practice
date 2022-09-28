daabd16fc8ea80dbe8360d4a69edff1cef16710e
Vasya Eremin <ervasyavc@gmail.com>
2022-09-28 16:37:10 +0300

    Lab0 complete

--- Lab0.c
+++ Lab0.c
@@ -9,11 +9,11 @@ int main(){
     printf("Введите координаты центра и радиус второй окружности: ");
     scanf("%i%i%i", &x2, &y2, &r2);
     distance = sqrt(pow(x1-x2, 2)+pow(y1-y2, 2));
-    if(x1==x2&y1==y2&r1==r2)
+    if(x1==x2 && y1==y2 && r1==r2)
         printf("Совпадают полностью");
-    else if(distance<r1-r2 | distance<r2-r1)
+    else if(distance<r1-r2 || distance<r2-r1)
         printf("Не пересекаются");
-    else if(distance==r1+r2 | distance==r1-r2 | distance==r2-r1)
+    else if(distance==r1+r2 || distance==r1-r2 || distance==r2-r1)
         printf("Пересекаются в 1 точке");
     else if(distance>r1+r2)
         printf("Не пересекаются");
