88491a8b984c64d9c6b07da5d30a300fb5f86969
Grudzin Konstantin <113104424+Konstantin-Grudzin@users.noreply.github.com>
2022-10-06 13:49:49 +0300

    Update HW.cpp

--- HW.cpp
+++ HW.cpp
@@ -3,24 +3,26 @@
 
 int main()
 {
-    float x1,x2,y1,y2,r1,r2;
+    float x1, x2, y1, y2, r1, r2;
 
-    printf("Center 1:");
-    scanf("%f %f %f",&x1,&y1,&r1);
-    printf("Center 2:");
-    scanf("%f %f %f",&x2,&y2,&r2);
+    printf("Center and radius 1:");
+    scanf("%f %f %f", &x1, &y1, &r1);
+    printf("Center and radius 2:");
+    scanf("%f %f %f", &x2, &y2, &r2);
 
-    if(sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))>(r1+r2))
+    float d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    if ( d > (r1 + r2))
     {
         printf("No touch");
     }
     else
     {
-        if(x1==x2 && y1==y2)
+        if (x1 == x2 && y1 == y2 && r1==r2)
         {
             printf("Sovpadenie");
         }
-        else if(sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))==(r1+r2))
+        else if (d == (r1 + r2) || d+r1==r2)
         {
             printf("1 touch");
         }
@@ -30,3 +32,4 @@ int main()
         }
     }
 }
+
